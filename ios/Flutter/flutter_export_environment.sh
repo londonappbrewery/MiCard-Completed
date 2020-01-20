b3a95ee6606db9d5752d36dee4e6d800b05ce0b0
TheMuellenator <TheMuellenator@users.noreply.github.com>
2020-01-20 11:23:15 +0000

    Updating gradle

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/philippmuellauer/Developer/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/philippmuellauer/Documents/Flutter/Flutter Complete/mi_card_flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/philippmuellauer/Downloads/MiCard-Completed"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
